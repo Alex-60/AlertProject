7968af7e33678f50a95987e436522b38e73f9da7
Alexandre <alex.legros60@gmail.com>
2020-02-13 14:21:20 +0100

    Ajout d'un autre style d'alert

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -15,12 +15,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func declencherAlerte(_ sender: UIButton) {
-        afficherAlert()
+        afficherAlertStyle1()
     }
     
-    func afficherAlert(){
+    @IBAction func declencherAlert2(_ sender: UIButton) {
+        afficherAlertStyle2()
+    }
+    
+    func afficherAlertStyle1(){
         
-        let myAlert = UIAlertController(title:"Mon Alerte", message: "Affichage de l'alerte", preferredStyle: .actionSheet)
+        let myAlert = UIAlertController(title:"Ma premiere alert", message: "Affichage de l'alerte", preferredStyle: .alert)
+        let btnAnnuler = UIAlertAction(title: "Annuler", style: .cancel, handler: nil)
+        let btnValider = UIAlertAction(title: "Valider", style: .default, handler: nil)
+        
+        myAlert.addAction(btnAnnuler)
+        myAlert.addAction(btnValider)
+        
+        present(myAlert, animated: true)
+    }
+    
+    func afficherAlertStyle2(){
+        
+        let myAlert = UIAlertController(title:"Ma seconde alerte", message: "Affichage de l'alerte", preferredStyle: .actionSheet)
         let btnAnnuler = UIAlertAction(title: "Annuler", style: .cancel, handler: nil)
         let btnValider = UIAlertAction(title: "Valider", style: .default, handler: nil)
         
